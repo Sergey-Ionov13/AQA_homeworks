495ef727aa74ecd12e047a14938dba784a7ca9f5
Sergey-Ionov13 <s.dyadya13@gmail.com>
2024-03-18 00:44:59 +0300

    Add and configure github ci workflow

--- calculator/.eslintrc.cjs
+++ calculator/.eslintrc.cjs
@@ -1,38 +1,63 @@
 module.exports = {
-    root: true,
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+  root: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+    'mocha': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'mocha'
+  ],
+  'rules': {
+    'indent': [2, 2],
+    'semi': [2, 'always'],
+    'no-extra-semi': 0,
+    'no-empty': 1,
+    'no-cond-assign': [2, 'always'],
+    camelcase: 1,
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: true,
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "mocha"
+    'no-console': [
+      'error',
+      {
+        allow: [
+          'warn',
+          'error',
+          'info',
+        ],
+      },
     ],
-    "rules": {
-        "indent": [2, 2],
-        "quotes": [2, "single"],
-        "semi": [2, "always"],
-        "no-extra-semi": 0,
-        "no-empty": 1,
-        "no-cond-assign": [2, "always"],
-    },
-}
+    'no-unused-vars': 1,
+    quotes: [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      },
+    ],
+  },
+};
